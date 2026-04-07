7397fc4d4ba734a92b741348169523f788de54bf
etud user <etud@localhost>
2026-04-07 10:11:16 +0000

    Updates

---
+++ assets/SectionTitle-Sk7_QfeC.js
@@ -0,0 +1,3 @@
+import{j as a}from"./index-DjYj_6ze.js";function f({children:t,className:e="",id:r}){return a.jsx("h2",{id:r,className:`text-2xl sm:text-3xl font-bold text-white relative inline-block
+        after:absolute after:bottom-[-6px] after:left-0 after:w-10 after:h-[3px]
+        after:bg-accent after:rounded-full ${e}`,children:t})}export{f as S};
